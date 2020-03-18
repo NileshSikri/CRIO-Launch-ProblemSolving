bad4d41a337e93940ce0903a3895b8f96e9d623f
NileshSikri <nilesh.kumarsikri@gmail.com>
2020-03-18 10:09:10 +0000

    [LocalTest]:[SpiralMatrix]:[java]

--- SpiralMatrix/SpiralMatrix.java
+++ SpiralMatrix/SpiralMatrix.java
@@ -4,9 +4,28 @@ import java.util.*;
 class SpiralMatrix {
     // Implement your solution by completing the below function
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> lst = new ArrayList<Integer>();
-        
-        return lst;
+        List ans = new ArrayList();
+        if (matrix.length == 0) return ans;
+        int R = matrix.length, C = matrix[0].length;
+        boolean[][] seen = new boolean[R][C];
+        int[] dr = {0, 1, 0, -1};
+        int[] dc = {1, 0, -1, 0};
+        int r = 0, c = 0, di = 0;
+        for (int i = 0; i < R * C; i++) {
+            ans.add(matrix[r][c]);
+            seen[r][c] = true;
+            int cr = r + dr[di];
+            int cc = c + dc[di];
+            if (0 <= cr && cr < R && 0 <= cc && cc < C && !seen[cr][cc]){
+                r = cr;
+                c = cc;
+            } else {
+                di = (di + 1) % 4;
+                r += dr[di];
+                c += dc[di];
+            }
+        }
+        return ans;
     }
 
     public static void main(String args[]) {
