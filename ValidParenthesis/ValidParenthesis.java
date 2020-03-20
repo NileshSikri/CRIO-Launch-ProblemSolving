8b7320d879dcae1b87fc8df16aebd387f592c870
NileshSikri <nilesh.kumarsikri@gmail.com>
2020-03-20 09:50:18 +0000

    [LocalTest]:[ValidParenthesis]:[java]

--- ValidParenthesis/ValidParenthesis.java
+++ ValidParenthesis/ValidParenthesis.java
@@ -4,8 +4,39 @@ import java.util.*;
 public class ValidParenthesis {
     // Implement your solution by completing the below function
     public boolean isValid(String s) {
+      int n = s.length();
+      Stack<Character> stk = new Stack<>(); 
+      for(int i=0;i<n;i++)
+      {
+        if(s.charAt[i]=='(' || s.charAt[i]=='{' || s.charAt[i]=='[')
+        stk.push(s.charAt[i]);
 
-		return false;
+        if(s.charAt[i]==')')
+        {
+          char a = stk.peek();
+          if(a=='(')
+          stk.pop();
+        }
+
+        if(s.charAt[i]=='}')
+        {
+          char a = stk.peek();
+          if(a=='{')
+          stk.pop();
+        }
+
+        if(s.charAt[i]==']')
+        {
+          char a = stk.peek();
+          if(a=='[')
+          stk.pop();
+        }
+      }
+
+      if(stk.empty())
+      return true;
+      else
+      return false;
     }
 
     public static void main(String[] args) throws IOException {
