086f9f2572f5756b80b65294c095d70f01da6133
NileshSikri <nilesh.kumarsikri@gmail.com>
2020-03-20 09:31:27 +0000

    [LocalTest]:[TwoSum]:[java]

--- TwoSum/TwoSum.java
+++ TwoSum/TwoSum.java
@@ -4,7 +4,18 @@ import java.util.*;
 public class TwoSum {
     // Implement your solution by completing the below function	
     public int[] twoSum(int[] nums, int target) {
-
+        int n = nums.length;
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<n;i++)
+        {
+            int c = target - nums[i];
+            if(map.containsKey(c))
+            {
+                return new int[] {i,map.get(c)};
+            }
+            map.put(nums[i],i);
+        }
+        
         return new int[0];
     }
 
